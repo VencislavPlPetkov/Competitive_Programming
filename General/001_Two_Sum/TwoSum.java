5916fb1f739a879dcb9a860a7ed9e87fc246d4e7
VencislavPlPetkov <v.pl.petkov@gmail.com>
2020-11-21 17:42:49 +0200

    Refactor

--- General/001_Two_Sum/TwoSum.java
+++ General/001_Two_Sum/TwoSum.java
@@ -1,60 +1,27 @@
-
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    
-    int[] results = new int[2]; 
-    
-    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
         
-    int counter = 0;
-        
-    for (int i = 0; i < nums.length; i++) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-        int numNeeded = target - nums[i];
+        int[] res = new int[2];
         
-        if (numNeeded == nums[i]){
+        for(int i = 0; i < nums.length; i++){
             
-             if (m.get(nums[i]) == null) {
-                m.put(nums[i], i); 
-             } else {
+            int numNeeded = target - nums[i];
+            
+            if(!map.containsKey(numNeeded)) {
                 
-                results[0] = m.get(numNeeded);
-                results[1] = i;
-                 
-             }
-        } else {
-            
-            
-        
-            System.out.print(numNeeded);
-
-            if(m.get(nums[i]) == null) {
-                m.put(nums[i], i); 
+                map.put(nums[i],i);
+                
+            } else {
+                
+                res[0] = i;
+                res[1] = map.get(numNeeded);
+                
             }
-
-
-
-            if(m.get(numNeeded) != null) {
-
-
-                results[0] = m.get(numNeeded);
-                results[1] = m.get(nums[i]);
-
-
-            }
-
-
+            
         }
-
-
-
-
-    } 
         
-        
-
-        return results;
-        
+        return res;        
     }
 }
--- General/002_Add_Two_Numbers/AddTwoNumbers.java
+++ General/002_Add_Two_Numbers/AddTwoNumbers.java
@@ -3,105 +3,39 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger; 
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        BigInteger num1 = listNodeToInt(l1);
-        BigInteger num2 = listNodeToInt(l2);
-           
-        BigInteger sum = num1.add(num2);
-         
-        ListNode res = intToListNode(sum);
+        ListNode result = new ListNode();
+        ListNode res = result;
         
-
-        return res;
+        int carry = 0;
         
-    }
-    
-    
-    // 2 -> 4 -> 3
-    // 342
-    public BigInteger listNodeToInt(ListNode l) {
-        
-        String numberStrRev = "";
-        ListNode p = l;
-        
-        
-        while (p != null){
+        while (l1 != null || l2 != null) {
             
-            numberStrRev += p.val;
-            p = p.next;
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+                
+            int sum = carry + val1 + val2;
+            carry = sum / 10;
+
+            res.next = new ListNode(sum%10);
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+            res = res.next;
             
         }
         
-		
-		String numberStrCorrect = new StringBuilder(numberStrRev).reverse().toString();
-        
-        // long num = Long.valueOf(numberStrCorrect).longValue();
-        
-        BigInteger num = new BigInteger(numberStrCorrect);
-        
-        return num;
-    }
-    
-    
-    public ListNode intToListNode (BigInteger num){
-        
-        String strRev = String.valueOf(num);
-        String strCorrect = new StringBuilder(strRev).reverse().toString();
-        
-        String firstChar = Character.toString(strCorrect.charAt(0));
-        int firstNum = Integer.parseInt(firstChar);                                      
-                                              
-                                              
-        
-        ListNode res = new ListNode(firstNum);
-        
-        int strLength = strCorrect.length();
-        
-        if (strLength == 2){
-            
-            String secChar = Character.toString(strCorrect.charAt(1));
-            int secNum = Integer.parseInt(secChar); 
-            
-            res.next = new ListNode(secNum);
-            
-        } else {
-        
-            ListNode p = res;
-
-            int counter = 1;
-
-
-            while (counter < strLength - 1) {
-
-                for (int i = 1; i < strLength; i++) {
-
-                    String curChar = Character.toString(strCorrect.charAt(i));
-                    int curNum = Integer.parseInt(curChar);  
-
-                    p.next = new ListNode(curNum);
-                    p = p.next;
-                counter++;
-
-                }
-
-
-            }
+        if(carry>0){
+            res.next= new ListNode(carry);
         }
         
-        return res;
+        return result.next;
     }
-
-    
-    
 }
-
-
-
-
