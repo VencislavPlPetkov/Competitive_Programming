c6ed6426c91244b7f0daf89a92afd6742f8cb23f
VencislavPlPetkov <v.pl.petkov@gmail.com>
2021-02-18 20:58:06 +0200

    Add

---
+++ General/074_Search_a_2D_Matrix/Search2DMatrix.java
@@ -0,0 +1,62 @@
+
+/**
+ * Search 2D Matrix
+ * 
+ * Write an efficient algorithm that searches for a value in an m x n matrix.  * This matrix has the following properties:
+ * 
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous  * row.
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * Output: false
+ */
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+        
+        int mLength = matrix.length;
+        int rowLength = matrix[0].length;
+        
+        int left = 0;
+        int right = mLength*rowLength - 1;
+        
+        int pivotIndex, pivotValue;
+        
+        while(left <= right){
+            
+            pivotIndex = (right+left)/2;
+            
+            pivotValue = matrix[pivotIndex / rowLength][pivotIndex % rowLength];
+            
+            if(pivotValue == target){
+                return true;
+            } else if(pivotValue < target ){
+                left = pivotIndex + 1;
+            } else if(pivotValue > target ){
+                right = pivotIndex - 1;
+            }
+            
+        }
+        
+   
+        return false;
+        
+    }
+}
+
+
+
+
